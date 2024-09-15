f96b417f587e3e8a3246772448ae7d1d0d380f46
moose@97 <ankushkol35@gmail.com>
2024-09-15 18:00:18 +0530

    bar seconds added

--- script/custom.js
+++ script/custom.js
@@ -1,15 +1,23 @@
 const numberHours = document.querySelector('.number-hours');
+const barSeconds = document.querySelector('.bar-seconds');
 
 const numberElement = [];
-
+const barElement = [];
 
 // create number hours
 for (let i = 1; i <= 12; i++){
     numberElement.push(
         `<span style="--index:${i};"><p>${i}</p></span>`
     );
-    console.log(numberElement);
 }
 
 numberHours.insertAdjacentHTML("afterbegin", numberElement.join(""));
 
+// create bar seconds
+for(let i = 1; i <= 60; i++){
+    barElement.push(
+        `<span style="--index:${i}"><p></p></span>`
+    );
+}
+
+barSeconds.insertAdjacentHTML("afterbegin", barElement.join(""));
